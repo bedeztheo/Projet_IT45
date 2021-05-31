ceb23c35327e2d4415f8074bab1f1242ec7da7ba
Theo Bedez <bedeztheo29@gmail.com>
2021-05-31 23:02:35 +0200

    structure individu

---
+++ structures.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTURES_H_INCLUDED
+#define STRUCTURES_H_INCLUDED
+
+typedef struct {
+
+    int idIndividu;
+    int idFormations[12];
+    int nbHeuresSemaine;
+
+} individu;
+
+#endif // STRUCTURES_H_INCLUDED
