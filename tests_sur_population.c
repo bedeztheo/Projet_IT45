f07910f636cb5bd71ecbc51ac5fc380de1e8ddff
AntoinePlanche <antoineplanche86@gmail.com>
2021-06-12 16:43:55 +0200

    ajout de commentaires de fonctions en Doxygen

--- tests_sur_population.c
+++ tests_sur_population.c
@@ -6,12 +6,25 @@
 #include "structures.h"
 #include "fonctions_pour_evaluation.h"
 
+/**
+ * @brief calcule le nombre d'heure que dure une formation
+ * @param idFormation l'id de la formation dont on calcule la durée
+ * @return le nombre d'heure que dure la formation
+ */
 int nombreHeureFormation(int idFormation){
 
     return formation[idFormation][5] - formation[idFormation][4];
 
 }
 
+/**
+ * @brief permet de savoir si une formation est compatible avec l'emploi du temps d'une interface.
+ * @param population la population qui contient l'individu qui contient l'interface
+ * @param idIndividu l'id de l'individu qui contient l'interface
+ * @param idInterface l'id de l'interface dont on calcule la compatibilité d'emploi du temps avec la nouvelle formation
+ * @param ifFormation l'id de la nouvelle formation dont on calcule la compatibilité avec l'emploi du temps de l'interface
+ * @return 1 si compatible, 0 sinon
+ */
 int formationCompatible(Population population, int idIndividu, int idInterface, int idFormation){
 
     int jourFormation = formation[idFormation][3], hDebutFormation = formation[idFormation][4], hFinFormation = formation[idFormation][5];
@@ -27,11 +40,16 @@ int formationCompatible(Population population, int idIndividu, int idInterface, 
         }
 
     }
-
     return 1;
 
 }
 
+/**
+ * @brief permet de trouver l'individu qui possède le meilleur fitness au sein d'une population
+ * @param population la population à étudier
+ * @param idIndividu l'id de l'individu qui contient l'interface
+ * @return l'id du meilleur individu
+ */
 int trouverMeilleurIndividu(Population population){
 
     float evalMeilleur = 100000;
